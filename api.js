fff660a7216541e8ff9ebfc3eabf5e2e0d6a5ef6
danielmenezesbr <danielmenezes@gmail.com>
2025-09-09 12:27:08 -0300

    Update api.js

--- api.js
+++ api.js
@@ -14,19 +14,19 @@ router.get('/', function (req, res) {
 });
 
 // readiness test
-router.get('/health', function (req, res) {
+router.get('/api/v2/health', function (req, res) {
   var returnData = {result: 'ok', version: version};
   res.status(200).send(returnData);
 });
 
 // echo service (deprecated in v2, warn the user)
-router.get('/echo/:msg?', function (req, res) {
+router.get('/api/v2/echo/:msg?', function (req, res) {
   var returnData = {result: 'warning', reason: 'this endpoint is deprecated, use say/ instead', msg: (req.params.msg || '')};
   res.status(200).send(returnData);
 });
 
 // say service
-router.get('/say/:msg?', function (req, res) {
+router.get('/api/v2/say/:msg?', function (req, res) {
   if (req.params.msg)
     var returnData = {result: 'ok', msg: req.params.msg };
   else
